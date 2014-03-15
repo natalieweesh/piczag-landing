8cef7df76d0adb1f8ab8a090f40c328069f81958
natalieweesh <natalieweesh@gmail.com>
2014-03-15 04:31:44 -0400

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,42 @@
-console.log('This would be the main JS file.');
+<script type="text/javascript" src="//ignition.launchrock.com/ignition-current.min.js"></script>
+
+var adjustIphone = function() {
+  var iphoneHeight = 569;
+  var windowHeight = $(window).innerHeight();
+  var iphoneMargin = (windowHeight - iphoneHeight)/2;
+  $('.iphone-container').css('margin-top', iphoneMargin + 'px');
+}
+var adjustSlides = function() {
+  var windowHeight = $(window).innerHeight();
+  $('.slide').css('min-height',windowHeight + 'px');
+}
+
+$(document).ready(function(){
+  adjustIphone();
+  adjustSlides();
+  $('.iphone-screen').css('opacity','0');
+  $('.iphone-screen[data-index=1]').css('opacity','1');
+
+  $(window).scroll(function(){
+    var distances = [];
+    $('.slide').each(function(idx){
+      dist = Math.abs($(window).scrollTop() - $(this).position().top);
+      distances.push(dist);
+      
+    });
+    var min = distances[0];
+    var minIdx = 0;
+    for (var j=0; j<distances.length; j++) {
+      if (distances[j] <= min) {
+        min = distances[j];
+        minIdx = j;
+      }
+    }
+    $('.iphone-screen').css('opacity','0');
+    $('.iphone-screen[data-index=' + (minIdx+1) + ']').css('opacity','1');
+  });
+});
+$(window).resize(function() {
+  adjustIphone();
+  adjustSlides();
+});
