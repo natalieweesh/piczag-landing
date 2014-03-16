da4637153dea0992e1d4c2eb297e82c409d6d2d2
natalieweesh <natalieweesh@gmail.com>
2014-03-15 23:28:51 -0400

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -15,6 +15,16 @@ $(document).ready(function(){
   $('.iphone-screen').css('opacity','0');
   $('.iphone-screen[data-index=1]').css('opacity','1');
 
+  $('section[data-type="background"]').each(function() {
+    var $bgobj = $(this);
+    $(window).scroll(function() {
+      var yPos = -(($(window).scrollTop() - $bgobj.position().top) / $bgobj.data('speed'));
+      var coords = '50% ' + yPos + 'px';
+      $bgobj.css({backgroundPosition: coords})
+    })
+
+  });
+
   $(window).scroll(function(){
     var distances = [];
     $('.slide').each(function(idx){
